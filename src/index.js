4733e6e9ab8384da5db85fabac7d7a769e675de0
La Thieu Huan <lathieuhuan@gmail.com>
2020-12-31 00:37:08 +0700

    day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: true };
+  }
+  delHeader = () => {
+    this.setState({ show: false });
+  };
+  render() {
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    }
+    return (
+      <div>
+        {myheader}
+        <button type="button" onClick={this.delHeader}>
+          Delete Header
+        </button>
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+ReactDOM.render(<Container />, document.getElementById("root"));
