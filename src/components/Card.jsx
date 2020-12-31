b685d56da20c157d754840809faa902c9190ec42
La Thieu Huan <lathieuhuan@gmail.com>
2020-12-31 20:22:28 +0700

    second try

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 import '../assets/css/Card.css'
 
+let chosen = [];
+
 class Card extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            faceUp: false,
-            isDone: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      faceUp: false,
+      isDone: false,
+    }
+  }
+  turnOver() {
+    if (!this.state.faceUp) {
+      this.setState({faceUp: true});
+    } else {
+      this.setState({faceUp: false});
+    }
+  }
+  render () {
+    // if (chosen.length === 2) {
+    //   this.setState({isDone: chosen[0] === chosen[1] ? true : false});
+    // }
+    return (
+      <div className={this.state.isDone ? "card invis" : "card"} data-no={this.props.cardNo} id={this.props.id}
+      onClick={() => {
+        this.turnOver();
+        if (chosen.length < 2) {
+          chosen.push(this.props.cardNo);
         }
-    }
-    turnOver() {
-        if (!this.state.faceUp) {
-            this.setState({faceUp: true});
-        } else {
-            this.setState({faceUp: false});
+        if (chosen.length === 2) {
+          this.setState({isDone: chosen[0] === chosen[1] ? true : false});
+          chosen = [this.props.cardNo];
         }
-    }
-    render () {
-        return (
-            <div class="card" data-no="no-${shuffled[i]}"></div>
-        )
-    }
+        console.log(chosen);
+        }}>
+        <img src={this.state.faceUp ? this.props.img : "cardback.jpg"} alt=""/>
+      </div>
+    )
+  }
 }
 
 export {Card}
