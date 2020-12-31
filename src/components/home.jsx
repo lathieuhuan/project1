36ac066f52a402a27f8a774b48a5c2d8fc4a0ccc
La Thieu Huan <lathieuhuan@gmail.com>
2021-01-01 00:15:09 +0700

    sample code

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../assets/css/home.css'
+// const front = 'https://upload.wikimedia.org/wikipedia/vi/thumb/f/f7/Sugimoris025.png/220px-Sugimoris025.png'
+export function Card(props) {
+    return (
+        <div className="card-area " onClick={() => props.flip(props.indx, 1)}>
+            <img className="card-image"
+                src={props.isFinish ? props.finish : props.isUp ? props.front : props.back}
+                flip={props.isFlip}
+                onAnimationEnd={() => { props.flip(props.indx, 0); props.select(props.indx) }}
+                alt="pikachu" />
+
+        </div >
+    )
+}
