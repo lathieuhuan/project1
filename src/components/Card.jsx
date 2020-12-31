86dcdb4fc272d6508b7afbfd9754c84ff47f3fdb
La Thieu Huan <lathieuhuan@gmail.com>
2021-01-01 00:00:24 +0700

    third try

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ class Card extends React.Component {
     super(props)
     this.state = {
       faceUp: false,
-      isDone: false,
+      isDone: false
     }
   }
   turnOver() {
@@ -19,23 +19,19 @@ class Card extends React.Component {
     }
   }
   render () {
-    // if (chosen.length === 2) {
-    //   this.setState({isDone: chosen[0] === chosen[1] ? true : false});
-    // }
     return (
       <div className={this.state.isDone ? "card invis" : "card"} data-no={this.props.cardNo} id={this.props.id}
-      onClick={() => {
-        this.turnOver();
-        if (chosen.length < 2) {
-          chosen.push(this.props.cardNo);
-        }
-        if (chosen.length === 2) {
-          this.setState({isDone: chosen[0] === chosen[1] ? true : false});
-          chosen = [this.props.cardNo];
-        }
-        console.log(chosen);
+        onClick={() => {
+          this.turnOver();
+          if (chosen.length < 2) {
+            chosen.push(this.props.cardNo);
+          }
+          if (chosen.length === 2) {
+            this.setState({isDone: chosen[0] === chosen[1] ? true : false});
+            chosen = [this.props.cardNo];
+          }
         }}>
-        <img src={this.state.faceUp ? this.props.img : "cardback.jpg"} alt=""/>
+        <img src={this.state.faceUp ? this.props.img : "cardback.jpg"} alt="" />
       </div>
     )
   }
