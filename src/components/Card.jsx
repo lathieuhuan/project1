b3b9e5ac67b5e99f39ffe736eb53c30ae4a187a4
La Thieu Huan <lathieuhuan@gmail.com>
2021-01-01 09:16:54 +0700

    not done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,9 +27,10 @@ let imgs = [
 ];
 let data = imgI.map((i) => {return {
   image: imgs[i],
-  faceUp: true,
+  back: "cardback.jpg",
+  faceUp: false,
   isDone: false,
-  needFlip: false
+  needFlip: 0
 }});
 // console.log(data);
 
@@ -40,13 +41,60 @@ class Playground extends React.Component {
       cards: data,
       chosen: []
     }
+    this.choose = this.choose.bind(this);
+    this.flip = this.flip.bind(this);
+    this.check = this.check.bind(this);
+  }
+  check() {
+    let data = this.state.cards;
+    let chosen = this.state.chosen;
+    let a = chosen[0];
+    let b = chosen[1];
+    setTimeout(() => {
+      if (data[a].image === data[b].image) {
+        data[a].isDone = true;
+        data[b].isDone = true;
+      } else {
+        data[a].faceUp = false;
+        data[b].faceUp = false;
+      }
+      this.setState({
+        cards: data,
+        choose: [],
+      });
+    }, 500);
+  }
+  choose(i) {
+    this.setState((state) => {
+      let data = JSON.parse(JSON.stringify(state.cards));
+      let chosen = JSON.parse(JSON.stringify(state.chosen));
+      if (!data[i].isDone) {
+        if (chosen.indexOf(i) === -1) {
+          chosen.push(i);
+        } else {
+          chosen = [];
+        }
+        data[i].faceUp = !data[i].faceUp;
+        return {
+          ...state,
+          data,
+          chosen,
+        };
+      }
+    });
   }
   flip (i, val) {
+    console.log("flip");
     this.setState((state) => {
       let data = state.cards;
       data[i].needFlip = val;
       return {cards: data};
     });
+  }
+  componentDidUpdate() {
+    if (this.state.chosen.length === 2) {
+      this.check();
+    }
   }
   render() {
     return <div id="playground">{
@@ -54,7 +102,8 @@ class Playground extends React.Component {
         return <Card key={i}
           {...val}
           index={i}
-          flip />; // here
+          flip={this.flip}
+          choose={this.choose} />;
       })
     }</div>
   }
@@ -62,8 +111,12 @@ class Playground extends React.Component {
 
 function Card(props) {
     return (
-      <div className="card" >
-        <img src={props.faceUp ? props.image : "cardback.jpg"} alt="card"/>
+      <div className={props.isDone ? "card invis" : "card"}
+      onClick={() => props.flip(props.index, 1)}>
+        <img src={props.faceUp ? props.image : props.back}
+        flip={props.needFlip}
+        onAnimationEnd={() => {props.flip(props.index, 0); props.choose(props.index)}}
+        alt="card"/>
       </div>
     )
 }
