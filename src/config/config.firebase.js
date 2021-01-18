c2566461087a0e4ff20c6d8e04437950b489e7e1
La Thieu Huan <lathieuhuan@gmail.com>
2021-01-18 23:33:45 +0700

    .

---
+++ src/config/config.firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbW16mrG9q2Qm9klH714Kcn86hAQf6_Mg",
+  authDomain: "first-cijs-d06.firebaseapp.com",
+  projectId: "first-cijs-d06",
+  storageBucket: "first-cijs-d06.appspot.com",
+  messagingSenderId: "192129456041",
+  appId: "1:192129456041:web:e289108c0d950b745f56b8",
+};
